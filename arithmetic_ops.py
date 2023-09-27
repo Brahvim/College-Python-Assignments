7e070ed596c6bbdf3de63a21405cfe7046bbcb96
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-27 21:56:51 +0530

    SYNTAX SUGAR INCOMING!

--- arithmetic_ops.py
+++ arithmetic_ops.py
@@ -1,22 +1,5 @@
 from numbers import Number
 
-# region Operators.
-# Finally! A place for me to use lambdas:
-operators = {
-    "+": lambda a, b: a + b,
-    "-": lambda a, b: a - b,
-    "*": lambda a, b: a * b,
-    "/": lambda a, b: a / b,
-    "%": lambda a, b: a % b,
-    "^": lambda a, b: a**b,
-    "//": lambda a, b: a // b,
-}  # ...An entire LANGUAGE to do that!
-
-# We need dis!!!:
-operators["**"] = operators["^"]
-# endregion
-
-
 print("Welcome to the arithmetic operations program!")
 print("This specific implementation is an interactive calculator.")
 print("Try entering expressions like `1234 + 5678`! (With the spaces!):")
@@ -26,27 +9,36 @@ while True:
 
     user_input = input()
     tokens = user_input.split(" ")
-    num_tokens = len(tokens)
 
-    if num_tokens != 3:
+    if (len(tokens)) != 3:
         print("That's not valid input!")
         continue
 
-    a = tokens[0]
-    b = tokens[2]
+    a, b = tokens[0], tokens[2]
 
     if not (a.isdigit() or b.isdigit()):
         print("Sorry - numbers only!")
         continue
 
-    a = float(a)
-    b = float(b)
+    a, b = float(a), float(b)
 
     result = "Oops! Looks like we can't calculate that!"
 
+    operators = {
+        "+": a + b,
+        "-": a - b,
+        "*": a * b,
+        "/": a / b,
+        "%": a % b,
+        "^": a**b,
+        "//": a // b,
+    }  # ...An entire LANGUAGE to do that!
+
+    operators["**"] = operators["^"]
+
     try:
         op = operators[tokens[1]]
-        result = op(a, b)
+        result = op  # (a, b)
         rounded = int(result)
 
         if result == rounded:
